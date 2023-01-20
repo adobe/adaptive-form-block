2be30c8f762c885644bf1399a7e4e142c7124587
Pareesh <paregupt@Pareeshs-MacBook-Pro.local>
2023-01-20 14:12:43 +0530

    Correcting eslint errors for radio

--- src/components/radio/radio.js
+++ src/components/radio/radio.js
@@ -1,27 +1,24 @@
-import {  getLayoutProperties, getTooltipValue, getViewId } from "../../libs/afb-model.js";
-import { getWidget, subscribe } from "../../libs/afb-interaction.js";
-import { Constants } from "../../libs/constants.js";
-import * as builder from "../../libs/afb-builder.js";
-import { Checkbox } from "../checkbox/checkbox.js";
+import { getWidget } from '../../libs/afb-interaction.js';
+import { Constants } from '../../libs/constants.js';
+import { Checkbox } from '../checkbox/checkbox.js';
 
 export class Radio extends Checkbox {
-
     blockName = Constants.RADIO;
 
     updateValue = (element, value) => {
-        let widget =  getWidget(element);
-        widget.checked = this.model.enum?.[0] == value 
+        const widget = getWidget(element);
+        widget.checked = this.model.enum?.[0] == value; // eslint-disable-line eqeqeq
         if (widget.checked) {
             widget.setAttribute(Constants.HTML_ATTRS.CHECKED, Constants.HTML_ATTRS.CHECKED);
-            widget.setAttribute(Constants.ARIA_CHECKED, true + "");
+            widget.setAttribute(Constants.ARIA_CHECKED, `${true}`);
         } else {
             widget.removeAttribute(Constants.HTML_ATTRS.CHECKED);
-            widget.setAttribute(Constants.ARIA_CHECKED, false + "");
+            widget.setAttribute(Constants.ARIA_CHECKED, `${false}`);
         }
-    }
+    };
 }
 
 export default async function decorate(block, model) {
-    let radio = new Radio(block, model);
+    const radio = new Radio(block, model);
     radio.render();
 }

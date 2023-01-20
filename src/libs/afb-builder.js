d79848a85b6767f0693cbaa7c519f0a8881616fe
Pareesh <paregupt@Pareeshs-MacBook-Pro.local>
2023-01-20 15:37:32 +0530

    Correcting lint errors for afb-builder

--- src/libs/afb-builder.js
+++ src/libs/afb-builder.js
@@ -1,5 +1,5 @@
-import * as defaultBuilder from './default-builder.js'
-import * as customerBuilder from '../customization/custom-builder.js'
+import * as defaultBuilder from './default-builder.js'; // eslint-disable-line import/no-cycle
+import * as customerBuilder from '../customization/custom-builder.js'; // eslint-disable-line import/no-cycle
 
-const result = {...defaultBuilder, ...customerBuilder}
+const result = { ...defaultBuilder, ...customerBuilder };
 export default result;

fbf6bc9c6414ab1fb884a502f99e8186c875cf23
Pareesh <paregupt@Pareeshs-MacBook-Pro.local>
2023-01-20 14:15:58 +0530

    Correcting linting errors for radio-group

--- src/components/radio-group/radio-group.js
+++ src/components/radio-group/radio-group.js
@@ -1,19 +1,18 @@
-import {  getLayoutProperties, getTooltipValue, getViewId } from "../../libs/afb-model.js";
-import { getWidget, subscribe } from "../../libs/afb-interaction.js";
-import { Constants } from "../../libs/constants.js";
-import * as builder from "../../libs/afb-builder.js";
+import { getLayoutProperties, getTooltipValue, getViewId } from '../../libs/afb-model.js';
+import { subscribe } from '../../libs/afb-interaction.js';
+import { Constants } from '../../libs/constants.js';
+import * as builder from '../../libs/afb-builder.js';
 
-import { DefaultField } from "../defaultInput.js";
+import { DefaultField } from '../defaultInput.js';
 
 export class RadioGroup extends DefaultField {
-
     blockName = Constants.RADIO;
 
     addListener() {
-        let widget =  this.element?.querySelectorAll(`[class$='${Constants.WIDGET}']`);
-        widget?.forEach(widget => {
+        const widgets = this.element?.querySelectorAll(`[class$='${Constants.WIDGET}']`);
+        widgets?.forEach((widget) => {
             widget.addEventListener('change', (e) => {
-                this.model.value =  e.target.value;
+                this.model.value = e.target.value;
             });
         });
     }
@@ -21,10 +20,10 @@ export class RadioGroup extends DefaultField {
     createInputHTML = (state) => {
         const fragments = document.createDocumentFragment();
         state?.enum?.forEach((enumVal, index) => {
-            fragments.appendChild(this.createRadioButton(state, enumVal, state?.enumNames?.[index], index))
-        })
+            fragments.appendChild(this.createRadioButton(state, enumVal, state?.enumNames?.[index], index));
+        });
         return fragments;
-    }
+    };
 
     createRadioButton = (state, enumValue, enumDisplayName, index) => {
         let div = document.createElement("div");
@@ -36,17 +35,17 @@ export class RadioGroup extends DefaultField {
         let label = document.createElement("label");
         label.className = this.blockName + "__option__label";
         label.title = getTooltipValue(state);
-        label.setAttribute("aria-label", enumDisplayName || enumValue);
-        label.setAttribute("aria-describedby", "_desc");
+        label.setAttribute('aria-label', enumDisplayName || enumValue);
+        label.setAttribute('aria-describedby', '_desc');
 
-        let input = builder?.default?.defaultInputRender(state, this.blockName)
-        input.type = "radio"
-        input.className = this.blockName + "__option__widget";
-        input.id = getViewId(state, this.blockName) + "__" + index + "__widget";
+        const input = builder?.default?.defaultInputRender(state, this.blockName);
+        input.type = 'radio';
+        input.className = `${this.blockName}__option__widget`;
+        input.id = `${getViewId(state, this.blockName)}__${index}__widget`;
         input.value = enumValue;
-        input.checked = enumValue == state.value;
+        input.checked = enumValue == state.value; // eslint-disable-line eqeqeq
 
-        let span = document.createElement("span");
+        const span = document.createElement('span');
         span.textContent = enumDisplayName || enumValue;
 
         label.appendChild(input);
@@ -54,48 +53,48 @@ export class RadioGroup extends DefaultField {
         div.appendChild(label);
 
         return div;
-    }
-    
+    };
+
     _updateEnum = (element, Enum) => {
-        //todo: remove extra options, fallback to enum if enumNames are not updated.
-        let widgets =  this.element?.querySelectorAll(`[class$='${Constants.WIDGET}']`);
-        widgets?.forEach((widget, i) =>{
+        // todo: remove extra options, fallback to enum if enumNames are not updated.
+        const widgets = this.element?.querySelectorAll(`[class$='${Constants.WIDGET}']`);
+        widgets?.forEach((widget, i) => {
             widget.value = Enum[i];
         });
-    }
+    };
 
     _updateEnumNames = (element, enumNames) => {
-        //todo: remove extra options, fallback to enum if enumNames are not updated.
-        let widgets =  this.element?.querySelectorAll(`[class$='option__label']`);
-        widgets?.forEach((widget, i) =>{
-            widget.querySelector("span").innerHTML = enumNames[i];
+        // todo: remove extra options, fallback to enum if enumNames are not updated.
+        const widgets = this.element?.querySelectorAll('[class$=\'option__label\']');
+        widgets?.forEach((widget, i) => {
+            widget.querySelector('span').innerHTML = enumNames[i];
         });
-    }
+    };
 
     updateValue = (element, value) => {
-        let widget =  element.querySelectorAll(`[class$='${Constants.WIDGET}']`);
-        widget?.forEach((widget) => {
-            if (widget.value != null && value?.toString() == widget.value.toString()) {
+        const widgets = element.querySelectorAll(`[class$='${Constants.WIDGET}']`);
+        widgets?.forEach((widget) => {
+            if (widget.value != null && value?.toString() == widget.value.toString()) { // eslint-disable-line eqeqeq
                 widget.checked = true;
                 widget.setAttribute(Constants.HTML_ATTRS.CHECKED, Constants.HTML_ATTRS.CHECKED);
-                widget.setAttribute(Constants.ARIA_CHECKED, true + "");
+                widget.setAttribute(Constants.ARIA_CHECKED, `${true}`);
             } else {
                 widget.checked = false;
                 widget.removeAttribute(Constants.HTML_ATTRS.CHECKED);
-                widget.setAttribute(Constants.ARIA_CHECKED, false + "");
+                widget.setAttribute(Constants.ARIA_CHECKED, `${false}`);
             }
-        }, this)
-    } 
+        }, this);
+    };
 
     render() {
-        this.element = builder?.default?.renderField(this.model, this.blockName, this.createInputHTML)
+        this.element = builder?.default?.renderField(this.model, this.blockName, this.createInputHTML);
         this.block.appendChild(this.element);
         this.addListener();
-        subscribe(this.model, this.element, {value : this.updateValue, enum: this._updateEnum, enumNames : this._updateEnumNames});
+        subscribe(this.model, this.element, { value: this.updateValue, enum: this._updateEnum, enumNames: this._updateEnumNames });
     }
 }
 
 export default async function decorate(block, model) {
-    let radio = new RadioGroup(block, model);
+    const radio = new RadioGroup(block, model);
     radio.render();
 }

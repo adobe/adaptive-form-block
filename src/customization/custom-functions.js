5bff5159aeb9f3c3264adf14184c7ac3305f3288
Pareesh <paregupt@Pareeshs-MacBook-Pro.local>
2023-01-20 15:17:50 +0530

    Move eslintrc and eslintingone to src folder. These files should also be part of afb block, to avoid linting errors on helix project

--- src/customization/custom-functions.js
+++ src/customization/custom-functions.js
@@ -4,15 +4,14 @@
  * @return {object} JSON Object after parsing the string as json. In case of
  * exceptions empty object is returned
  */
- function toObject(str) {
+function toObject(str) {
     try {
         return JSON.parse(str);
-    }
-    catch (e) {
-        return {}
+    } catch (e) {
+        return {};
     }
 }
 
 export const customFunctions = {
-    toObject
-}
+    toObject,
+};

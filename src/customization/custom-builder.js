4a86c11e26902910dbc1c05c629cdb98e9c56314
vdua <vdua@adobe.com>
2023-01-23 14:09:04 +0530

    fixing eslint issues

--- src/customization/custom-builder.js
+++ src/customization/custom-builder.js
@@ -1,15 +1,17 @@
-import {createLabel as cl} from '../libs/default-builder.js'
+/* eslint-disable import/prefer-default-export */
+// eslint-disable-next-line import/no-cycle
+import { createLabel as cl } from '../libs/default-builder.js';
 
 /**
  * Example of overriding to inlcude start
- * @param {*} state 
- * @param {*} bemBlock 
- * @returns 
+ * @param {*} state
+ * @param {*} bemBlock
+ * @returns
  */
 export const createLabel = (state, bemBlock) => {
-    const label = cl(state, bemBlock)
-    if(label) {
-        label.textContent = state?.required ? label?.textContent + " *" : label?.textContent;
-        return label;
-    }
-}
+  const label = cl(state, bemBlock);
+  if (label) {
+    label.textContent = state?.required ? `${label?.textContent} *` : label?.textContent;
+  }
+  return label;
+};
